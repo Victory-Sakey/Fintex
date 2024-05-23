636f79b8607a5022b61570b34c1bdf5b2fb3aea3
Victory-Sakey <victorysakey66@gmail.com>
2024-05-23 14:50:32 -0700

    Done

--- Crest/settings.py
+++ Crest/settings.py
@@ -33,7 +33,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-10dmq8(l=jzue@b&&7+dra4#y03ywk*s&6h7t6=6c%$0muv8+&'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
